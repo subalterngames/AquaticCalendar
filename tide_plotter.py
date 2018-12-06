91f9aaa3aa2045cb41fe8f63972a6446f64171af
subalterngames <seth@neuroscouting.com>
2018-12-05 20:14:42 -0500

    days

--- tide_plotter.py
+++ tide_plotter.py
@@ -1,6 +1,35 @@
 from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
+from json import loads, dumps
+
+DAYS_PER_MONTH = 30
+MONTHS = ["Tishrei", "Kheshvan", "Kislev", "Tevet", "Shvat", "Adar", "Nisan", "Iyar", "Sivan", "Tammuz", "Av", "Elul"]
+current_month_index = 0
+current_day_of_month = 0
+
+
+class Day:
+    def __init__(self, start, end, day_of_month, month, day_of_week, heights, yom_tov = ""):
+        """
+        A day in the Aquatic Jewish lunitidal calendar.
+
+        :param start: Start datetime (Gregorian)
+        :param end: End datetime (Gregorian)
+        :param day_of_month: The day of the aquatic month.
+        :param month: The aquatic month.
+        :param day_of_week: The day of the aquatic week.
+        :param heights: The tidal height data.
+        :param yom_tov: Name of yom tov (Default=empty).
+        """
+
+        self.start = start
+        self.end = end
+        self.day_of_month = day_of_month
+        self.month = month
+        self.day_of_week = day_of_week
+        self.heights = heights
+        self.yom_tov = yom_tov
 
 
 def get_start_time():
@@ -9,7 +38,8 @@ def get_start_time():
             return i
     return -1
 
-def get_next_day(start_time):
+
+def get_t1(start_time):
     got_middle_high_tide = False
     for i in range(start_time + 1, len(heights) - 1):
         if heights[i] > heights[i - 1] and heights[i] > heights[i + 1]:
@@ -18,17 +48,6 @@ def get_next_day(start_time):
             else:
                 got_middle_high_tide = True
     return -1
-
-
-def gregorian_to_aquatic(date):
-    start_of_calendar = datetime(2018, 9, 9, 3)
-    elapsed = timedelta()
-    elapsed.total_seconds()
-    elapsed = (date - start_of_calendar).total_seconds() / 60.0 / 60.0
-    print(elapsed)
-    months = ["Tishrei"]
-
-    print(date)
 
 
 def plot(day):
@@ -43,7 +62,7 @@ def plot(day):
     plt.setp(lines, linewidth=10)
     plt.tick_params(axis="both", which="both", bottom=False, top=False, left=False, right=False,
                     labelbottom=False, labelleft=False)
-    plt.show()
+    # plt.show()
 
 
 heights = []
@@ -61,10 +80,23 @@ heights = np.array(heights)
 t = np.array(t)
 
 t0 = get_start_time()
-t1 = get_next_day(t0)
+t1 = get_t1(t0)
 
-plot(heights[t0:t1])
-print(t[t0])
+days = []
+
+done = False
+while not done:
+    current_day_of_month += 1
+    if current_day_of_month > DAYS_PER_MONTH - 1:
+        current_day_of_month = 0
+        current_month_index += 1
+        if current_month_index > len(MONTHS) - 1:
+            done = True
+    if not done:
+        day = Day(t[t0], t[t1], current_day_of_month, MONTHS[current_month_index], "?", heights[t0 : t1])
+        days.append(day)
+        t0 = t1
+print(len(days))
 
 """
 # Absolute starting time.
